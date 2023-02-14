02a84e8bf2c9378a0b1b6fe8ad3796799d0edcee
Luar <luarrezende@gmail.com>
2023-02-14 18:41:09 -0300

    resolve requisito 1 3 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (element) => {
+  if (element === undefined) {
+    throw new Error('Termo de busca não informado');
+  }
+  const api = `https://api.mercadolibre.com/sites/MLB/search?q=${element}`;
+  const searchInApi = await fetch(api);
+  const result = await searchInApi.json();
+  return result.results;
 };
