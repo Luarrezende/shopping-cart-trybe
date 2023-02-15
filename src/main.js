49ed09ee62e2745f77d3238515b0767d39da0a6a
Luar <luarrezende@gmail.com>
2023-02-15 17:17:32 -0300

    resolve requisitos 3,4 e 5

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
+import './style.css';
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import './style.css';
 import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const products = document.querySelector('.products');
+
+const productsApi = async () => {
+  const div = document.createElement('div');
+  div.classList.add('loading');
+  div.innerHTML = 'carregando...';
+  products.appendChild(div);
+  try {
+    const sequencias = await fetchProductsList('computador');
+    products.innerHTML = '';
+    sequencias.forEach((sequencia) => {
+      const productHtml = createProductElement(sequencia);
+      products.appendChild(productHtml);
+    });
+  } catch (error) {
+    const erro = document.createElement('div');
+    erro.classList.add('error');
+    erro.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    products.innerHTML = '';
+    products.appendChild(erro);
+  }
+};
+
+window.onload = () => {
+  productsApi();
+};
