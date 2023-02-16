94df794528801ff4df141aa635ac89821fa7b037
Luar <luarrezende@gmail.com>
2023-02-16 19:22:01 -0300

    resolve requisito 8

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -121,6 +122,13 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'product__add',
     'Adicionar ao carrinho!',
   );
+  cartButton.addEventListener('click', async () => {
+    saveCartID(id);
+    const fetch = await fetchProduct(id);
+    const element = await createCartProductElement(fetch);
+    const products = document.querySelector('.cart__products');
+    products.appendChild(element);
+  });
   section.appendChild(cartButton);
 
   return section;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+// import { saveCartID } from './helpers/cartFunctions';
 import './style.css';
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+// const itemCart = document.querySelector('.cart__products');
 const products = document.querySelector('.products');
+// const total = document.querySelector('.total-price');
+// const btnsProducts = document.getElementsByClassName('product__add');
 
 const productsApi = async () => {
   const div = document.createElement('div');
